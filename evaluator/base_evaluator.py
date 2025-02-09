43c0efa88b3486ea3cbd1e0b0a29db4f3e89d030
KaiLi0822 <33022992+KaiLi0822@users.noreply.github.com>
2025-02-09 11:38:08 -0800

    Update evaluator/base_evaluator.py
    Co-authored-by: Muzhou Liu <sllzhlv77@gmail.com>

--- evaluator/base_evaluator.py
+++ evaluator/base_evaluator.py
@@ -59,7 +59,7 @@ class RAGEvaluator(ABC):
         """
         pass
     
-    def evaluate(self, answer: str, question: str = "", context: str = "",  **kwargs) -> Dict[str, float]:
+    def evaluate(self, answer: str|List[str], question: str|List[str] = None, context: str|List[str] = None, **kwargs ) -> Dict[str, float]:
         """
         Main evaluation workflow.
         
